caadc39df22e1fcb120d3376c9323e203d2c7cd2
castellano17 <castellano17@gmail.com>
2023-02-13 17:37:43 -0600

    change imagen size

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/UserCard.css";
 
 const UserCard = ({
   user,
@@ -11,28 +12,31 @@ const UserCard = ({
     handleClickShowModal();
   };
   return (
-    <article>
-      <h3>
+    <article className="UserCard">
+      <h3 className="UserCard__name">
         {user.first_name} {user.last_name}
       </h3>
       <hr />
-      <ul>
-        <li>
+      <ul className="UserCard__item">
+        <li className="UserCard__email">
           <span>Email:</span>
           {user.email}
         </li>
-        <li>
+        <li className="UserCard__birthday">
           <span>Birthday:</span> <i className="bx bx-gift"></i>
           {user.birthday}
         </li>
       </ul>
-      <hr />
-      <footer>
-        <button onClick={() => deleteUser(user.id)}>
+      <hr className="UserCard__line" />
+      <footer className="UserCard__btn-container">
+        <button
+          className="UserCard__btn-erase"
+          onClick={() => deleteUser(user.id)}
+        >
           <i className="bx bx-trash"></i>
         </button>
 
-        <button onClick={handleClickEdit}>
+        <button className="UserCard__btn-edit" onClick={handleClickEdit}>
           <i className="bx bx-pencil"></i>
         </button>
       </footer>
